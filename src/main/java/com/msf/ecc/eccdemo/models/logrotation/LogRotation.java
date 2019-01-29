e6b512bbeab7f7621e9f02d81cb7958ab29dcba9
praveen-vp <praveen.vp@inxsasia.com>
2019-01-29 14:38:02 +0530

    Adding changes for ecc

---
+++ src/main/java/com/msf/ecc/eccdemo/models/logrotation/LogRotation.java
@@ -0,0 +1,9 @@
+package com.msf.ecc.eccdemo.models.logrotation;
+
+/**
+ * @author praveen-vp
+ * 24/1/19
+ * com.msf.ecc.eccdemo.models.logrotation
+ */
+public class LogRotation {
+}
---
+++ src/test/java/com/msf/ecc/eccdemo/models/logrotation/LogRotationTest.java
@@ -0,0 +1,22 @@
+package com.msf.ecc.eccdemo.models.logrotation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author praveen-vp
+ * 24/1/19
+ * com.msf.ecc.eccdemo.models.logrotation
+ */
+public class LogRotationTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void checkIfRotationRequired() {
+    }
+}
